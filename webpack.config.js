7a0cf0936b659634cdc60eafda4d2e29221da670
Turbinist <srudyjju@gmail.com>
2018-08-14 00:24:33 +0300

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'app.bundle.js',
     publicPath: ''
   },
